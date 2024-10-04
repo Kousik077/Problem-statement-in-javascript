fc45002c7c8059b2ce434e6e0e924dada24206b6
kousik <122795004+Kousik077@users.noreply.github.com>
2024-10-04 11:14:57 +0530

    Add files via upload
    A toy factory is packing toys into boxes. Each box can contain either a toy (represented by an integer value) or be empty (represented by 0). Write a function to move all empty boxes to the end of the array while maintaining the order of the toys, and also return the count of empty boxes.
    Sample Input :
    Ex : Boxes = [1,0,2,0,3,4,0,5]

---
+++ toyfactory.js
@@ -0,0 +1,10 @@
+let arr=[10,9,0,7,6,4,0,2,0,1];
+let arr1= new Array(arr.length).fill(0);
+
+let j=0;
+for(let i=0;i<arr.length;i++){
+if(arr[i]!=0){
+    arr1[j]=arr[i];
+    j=j+1;
+}
+}console.log(arr1);
